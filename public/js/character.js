3c03ead73994d8e9b688fa2f2a808612d8be27dc
HongYuRi <mab6r6shi@gmail.com>
2026-07-24 17:43:43 +0900

    charcater.js 오류 해결 및 neon effect 변경

--- public/js/character.js
+++ public/js/character.js
@@ -1,57 +1,17 @@
 document.addEventListener(
-"DOMContentLoaded",
-()=>{
-
-
-const character =
-document.querySelector(".character-img");
-
-
-if(!character) return;
-
-
-
-// 등장 효과
-
-character.classList.add("character-enter");
-
-
-
-// 클릭 반응
-
-document
-.querySelectorAll(".menu-btn")
-.forEach(btn=>{
-
-
-btn.addEventListener(
-"mouseenter",
-()=>{
-
-
-character.classList.add(
-"character-active"
-);
-
-
-});
-
-
-btn.addEventListener(
-"mouseleave",
-()=>{
-
-
-character.classList.remove(
-"character-active"
-);
-
-
-});
-
-
-});
-
-
-
-});
+    "DOMContentLoaded",
+    ()=>{
+    
+    const character =
+    document.querySelector(".character-img");
+    
+    
+    if(!character) return;
+    
+    
+    // 등장 효과
+    
+    character.classList.add("character-enter");
+    
+    
+    });
